427eec109779c463e0d6ef4b6f13d7feb88a5364
Decamiron <vlad.krutik2002@gmail.com>
2022-05-26 23:43:01 +0300

    Revert "added wagtail integration"
    This reverts commit 19588f9f57aaf5b0870b7969e306cf60772002fb.

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -1,22 +1,27 @@
-from django.conf import settings
+"""moviesite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from main.views import pageNotFound, serverError
 from django.views.static import serve
 
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
-
-from main.views import pageNotFound, serverError
-from search import views as search_views
-
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
-
-    path('admin/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
-
+    path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('profile/', include('users.urls')),
     path('captcha/', include('captcha.urls'))
@@ -24,31 +29,14 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
 
-
 if settings.DEBUG:
     import debug_toolbar
     import mimetypes
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     mimetypes.add_type("application/javascript", ".js", True)
-
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
 
-
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
 
 handler404 = pageNotFound
 handler500 = serverError
