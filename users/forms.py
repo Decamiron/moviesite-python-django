48cf9c491fd3790f17c09d5d54219214c5f02c7a
Decamiron <vlad.krutik2002@gmail.com>
2022-05-30 23:27:56 +0300

    fixed registration form

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,5 @@
+from typing import re
+
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django import forms
 from users.models import MyUser, FilmUsersInfo
@@ -8,22 +10,76 @@ clases_for_css = 'border border-primary form-control bg-dark text-warning'
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(
-        attrs={'class': clases_for_css, "placeholder": "Username"}))
-    password1 = forms.CharField(label='Пароль',
-                                widget=forms.PasswordInput(attrs={'class': clases_for_css}))
-    password2 = forms.CharField(label='Подтверждение пароля',
-                                widget=forms.PasswordInput(attrs={'class': clases_for_css}))
-    email = forms.EmailField(label='Email',
-                             widget=forms.TextInput(attrs={'class': clases_for_css}))
-    about_user = forms.CharField(label='О себе', widget=forms.Textarea(attrs={'rows': 10, 'cols': 60, 'class': 'border border-primary bg-dark text-warning'}))
-    birth_date = forms.DateField(label='День Рождения', widget=forms.DateInput(attrs={'class': 'border border-primary bg-dark text-warning', "placeholder": "dd.mm.yyyy"}))
-    age = forms.IntegerField(label='Возраст', widget=forms.NumberInput(attrs={'class': 'bg-dark border border-primary text-warning'}))
+    username = forms.CharField(
+        label='Логин',
+        widget=forms.TextInput(
+           attrs={
+               'class': clases_for_css,
+               "placeholder": "Username"
+           }))
+
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': clases_for_css
+            }))
+
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': clases_for_css
+            }))
+
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.TextInput(
+            attrs={
+                'class': clases_for_css
+            }))
+
+    about_user = forms.CharField(
+        label='О себе',
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'rows': 10,
+                'cols': 60,
+                'class': 'border border-primary bg-dark text-warning',
+            }))
+
+    birth_date = forms.DateField(
+        label='День Рождения (Только от 18 лет)',
+        required=False,
+        help_text="Только от 18 лет",
+        widget=forms.DateInput(
+            attrs={
+                'class': 'border border-primary bg-dark text-warning',
+                "placeholder": "dd.mm.yyyy",
+            }))
+
+    first_name = forms.CharField(
+        label='Имя',
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'border border-primary bg-dark text-warning'
+            }))
+
+    last_name = forms.CharField(
+        label='Фамилия',
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'border border-primary bg-dark text-warning'
+            }))
+
     captcha = CaptchaField()
 
     class Meta:
         model = MyUser
-        fields = ('username', 'email', 'password1', 'password2', 'photo', 'birth_date', 'age', 'about_user', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password1', 'password2', 'photo', 'birth_date', 'about_user', 'first_name', 'last_name')
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -42,6 +98,11 @@ class FilmListView(forms.ModelForm):
         model = FilmUsersInfo
         fields = ('filmlist',)
 
-    filmlist = forms.ChoiceField(label='', choices=FilmUsersInfo.FILMLIST, initial='',
-                               widget=forms.Select(attrs={'class': 'form-select w-100 bg-dark text-danger my-2'}))
-
+    filmlist = forms.ChoiceField(
+        label='',
+        choices=FilmUsersInfo.FILMLIST,
+        initial='',
+        widget=forms.Select(
+            attrs={
+                'class': 'form-select w-100 bg-dark text-danger my-2'
+            }))
